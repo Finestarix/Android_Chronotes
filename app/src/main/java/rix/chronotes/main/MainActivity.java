4f4964102319b77cd3c11f96e43c181fd055baec
Finestarix <irenaldyleonarto@gmail.com>
2020-04-08 22:19:57 +0700

    Initial Android Project with Firebase Configuration

---
+++ app/src/main/java/rix/chronotes/main/MainActivity.java
@@ -0,0 +1,16 @@
+package rix.chronotes.main;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import rix.chronotes.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
